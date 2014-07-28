95669a965bf9877b0b4ea82f07316579135e4b6a
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-07-28 12:44:53 +0200

    introduced mongo

--- testblescan.py
+++ testblescan.py
@@ -6,6 +6,11 @@ import sys
 
 import bluetooth._bluetooth as bluez
 
+from pymongo import MongoClient
+mongo_client = MongoClient('mongodb://54.77.67.205:27017/')
+db = mongo_client.InnovationLab
+beacons_collection = db.beacons
+
 dev_id = 0
 try:
 	sock = bluez.hci_open_dev(dev_id)
@@ -19,8 +24,6 @@ blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
 while True:
-	returnedList = blescan.parse_events(sock, 10)
-	print "----------"
-	for beacon in returnedList:
-		print beacon
+	beacons = blescan.parse_events(sock, 10)
+	beacons_collection.insert(beacons)
 
