66da878dca6c9b7771e15e363713df9362368570
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-07-29 13:04:31 +0200

    playing with the payload

--- node/app.js
+++ node/app.js
@@ -21,10 +21,10 @@ MongoClient.connect("mongodb://127.0.0.1:27017/InnovationLab", function(error, d
     var note = new apn.Notification();
 
     note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-    note.badge = 3;
-    note.sound = "ping.aiff";
-    note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-    note.payload = {'messageFrom': 'Caroline'};
+    note.badge = 0;
+    //note.sound = "ping.aiff";
+    note.alert = "Your kid died. Go get a new one.";
+    //note.payload = {'messageFrom': 'Caroline'};
 
     apnConnection.pushNotification(note, myDevice);
 
