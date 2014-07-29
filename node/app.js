482962294feadb6029feb8a05d6cd983c0b43dcd
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-07-29 13:17:00 +0200

    notifies when the kid is far

--- node/app.js
+++ node/app.js
@@ -10,25 +10,9 @@ var log = new Log("debug");
 MongoClient.connect("mongodb://127.0.0.1:27017/InnovationLab", function(error, db) {
     if(error) throw error;
 
+    var notified = false;
+
     var beacons_collection = db.collection("beacons");
-
-    // send test push notification
-    log.debug("Starting notifications...");
-    var options = {};
-    var apnConnection = new apn.Connection(options);
-    var token = "7e973adab79134fb35587ac35dd8c13b2556530b4382b7934a0ef52dfd499e9c";
-    var myDevice = new apn.Device(token);
-    var note = new apn.Notification();
-
-    note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-    note.badge = 0;
-    //note.sound = "ping.aiff";
-    note.alert = "Your kid died. Go get a new one.";
-    //note.payload = {'messageFrom': 'Caroline'};
-
-    apnConnection.pushNotification(note, myDevice);
-
-    log.debug("Notification sent");
 
     //setInterval(check, 2000);
 
@@ -36,9 +20,34 @@ MongoClient.connect("mongodb://127.0.0.1:27017/InnovationLab", function(error, d
         beacons_collection.findOne({minor: 513}, function (error, result) {
             if (error || !result) return;
             if (result.rssi < -90) {
-                // Notify!!!
-                log.error("KID FAR AWAY!!!");
+                if (!notified) {
+                    log.debug("About to notify");
+                    notify();
+                }
+            }
+            else {
+                notified = false;
             }
         });
+    }
+
+    function notify() {
+        var options = {};
+        var apnConnection = new apn.Connection(options);
+        var token = "7e973adab79134fb35587ac35dd8c13b2556530b4382b7934a0ef52dfd499e9c";
+        var myDevice = new apn.Device(token);
+        var note = new apn.Notification();
+
+        note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+        note.badge = 0;
+        //note.sound = "ping.aiff";
+        note.alert = "Your kid died. Go get a new one.";
+        //note.payload = {'messageFrom': 'Caroline'};
+
+        apnConnection.pushNotification(note, myDevice);
+
+        notified = true
+
+        log.debug("Notification sent");
     }
 });
