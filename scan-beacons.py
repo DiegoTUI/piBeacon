04acc4c8cf176848816a631623e86d97b14fc784
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-09-16 10:27:39 +0200

    fixed indentation

--- scan-beacons.py
+++ scan-beacons.py
@@ -22,7 +22,7 @@ try:
     print "ble thread started"
 except:
     print "error accessing bluetooth device..."
-        sys.exit(1)
+    sys.exit(1)
 
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
